e000ff3b1e63875759311271690677be7ccb18c9
Siebe Michiels <r0901813@student.thomasmore.be>
2024-11-14 21:01:02 +0100

    Fixed issue #1

--- src/main/java/engine/graphics/Window.java
+++ src/main/java/engine/graphics/Window.java
@@ -98,7 +98,7 @@ public class Window {
         GL.createCapabilities();
 
         glEnable(GL_BLEND);
-        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
         this.imGuiLayer = new ImGuiLayer(glfwWindow);
         this.imGuiLayer.initImGui();
